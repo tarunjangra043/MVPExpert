98ee2b9a2f012ca336d1182bde51ce25ae54c059
Tarun Jangra <tarunjangra043@gmail.com>
2025-06-07 16:28:50 +0530

    starting

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import "./App.css";
+import { motion } from "motion/react";
+import { HeroHighlight, Highlight } from "./components/ui/HeroHighlight.jsx";
+
+import {
+  Navbar,
+  NavBody,
+  NavItems,
+  MobileNav,
+  NavbarLogo,
+  NavbarButton,
+  MobileNavHeader,
+  MobileNavToggle,
+  MobileNavMenu,
+} from "./components/ui/Navbar.jsx";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      {/* <div className="relative w-full">
+        <Navbar>
+          <NavBody>
+            <NavbarLogo />
+            <NavItems items={navItems} />
+            <div className="flex items-center gap-4">
+              <NavbarButton variant="secondary">Login</NavbarButton>
+              <NavbarButton variant="primary">Book a call</NavbarButton>
+            </div>
+          </NavBody>
+
+          <MobileNav>
+            <MobileNavHeader>
+              <NavbarLogo />
+              <MobileNavToggle
+                isOpen={isMobileMenuOpen}
+                onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+              />
+            </MobileNavHeader>
+
+            <MobileNavMenu
+              isOpen={isMobileMenuOpen}
+              onClose={() => setIsMobileMenuOpen(false)}
+            >
+              {navItems.map((item, idx) => (
+                <a
+                  key={`mobile-link-${idx}`}
+                  href={item.link}
+                  onClick={() => setIsMobileMenuOpen(false)}
+                  className="relative text-neutral-600 dark:text-neutral-300"
+                >
+                  <span className="block">{item.name}</span>
+                </a>
+              ))}
+              <div className="flex w-full flex-col gap-4">
+                <NavbarButton
+                  onClick={() => setIsMobileMenuOpen(false)}
+                  variant="primary"
+                  className="w-full"
+                >
+                  Login
+                </NavbarButton>
+                <NavbarButton
+                  onClick={() => setIsMobileMenuOpen(false)}
+                  variant="primary"
+                  className="w-full"
+                >
+                  Book a call
+                </NavbarButton>
+              </div>
+            </MobileNavMenu>
+          </MobileNav>
+        </Navbar>
+        <DummyContent />
+      </div> */}
+
+      <div>
+        <HeroHighlight>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              y: 20,
+            }}
+            animate={{
+              opacity: 1,
+              y: [20, -5, 0],
+            }}
+            transition={{
+              duration: 0.5,
+              ease: [0.4, 0.0, 0.2, 1],
+            }}
+            className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+          >
+            With insomnia, nothing&apos;s real. Everything is far away.
+            Everything is a{" "}
+            <Highlight className="text-black dark:text-white">
+              copy, of a copy, of a copy.
+            </Highlight>
+          </motion.h1>
+        </HeroHighlight>
+      </div>
+    </>
+  );
+}
+
+export default App;
